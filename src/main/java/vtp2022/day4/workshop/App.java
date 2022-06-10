fbba8900be325affe3537788a823fc97876fc18d
yingqing-c <yingqing.c@hotmail.com>
2022-06-10 10:05:25 +0800

    initial commit

---
+++ src/main/java/vtp2022/day4/workshop/App.java
@@ -0,0 +1,13 @@
+package vtp2022.day4.workshop;
+
+/**
+ * Main program
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
